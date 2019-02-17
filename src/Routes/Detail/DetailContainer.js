44646e8719100d0361e4020b7f06f59cf0b61d89
bonggt <kepy1106@gmail.com>
2019-02-17 18:04:54 +0900

    Containers[5.7_DetailContainer - destructuring assignment]

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -1,12 +1,20 @@
 import React from "react";
 import DetailPresenter from "./DetailPresenter";
+import { moviesApi, tvApi } from "../../api";
 
 export default class extends React.Component {
-  state = {
-    result: null,
-    error: null,
-    loading: true,
-  };
+  constructor(props) {
+    super(props);
+    const {
+      location: { pathname },
+    } = props;
+    this.state = {
+      result: null,
+      error: null,
+      loading: true,
+      isMovie: pathname.includes("/movie/"),
+    };
+  }
 
   async componentDidMount() {
     const {
@@ -15,17 +23,42 @@ export default class extends React.Component {
       },
       history: { push },
     } = this.props;
-    console.log(this.props);
+    const { isMovie } = this.state;
     const parsedId = parseInt(id);
     if (isNaN(parsedId)) {
       return push("/");
     }
+    let result = null;
+    try {
+      // const { data } = isMovie
+      //   ? await moviesApi.movieDetail(parsedId)
+      //   : await tvApi.showDetail(parsedId);
+      // result = data;
+
+      if (isMovie) {
+        ({ data: result } = await moviesApi.movieDetail(parsedId));
+      } else {
+        ({ data: result } = await tvApi.showDetail(parsedId));
+      }
+      console.log(result);
+    } catch {
+      this.setState({
+        error: "Can't find this movie & tv",
+      });
+    } finally {
+      this.setState({
+        loading: false,
+        result,
+      });
+    }
   }
 
   render() {
-    // console.log(this.props);
-
     const { result, error, loading } = this.state;
+
+    // console.log(this.props);
+    // console.log(this.state);
+    console.log(result);
 
     return <DetailPresenter result={result} error={error} loading={loading} />;
   }
