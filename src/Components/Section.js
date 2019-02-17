2a95d7bf24168560580befc8bfff89f1d7b86917
Bonggt <kepy1106@gmail.com>
2019-02-17 22:52:11 +0900

    Presenters[6.1_Section & propTypes.children]

---
+++ src/Components/Section.js
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const Container = styled.div`
+  :not(:last-child) {
+    margin-bottom: 32px;
+  }
+`;
+
+const Title = styled.span`
+  font-size: 24px;
+  font-weight: 600px;
+`;
+
+const Grid = styled.div`
+  margin-top: 16px;
+`;
+
+const Section = ({ title, children }) => (
+  <Container>
+    <Title>{title}</Title>
+    <Grid>{children}</Grid>
+  </Container>
+);
+
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]),
+};
+
+export default Section;
--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,6 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Section from "Components/Section";
+
+const Container = styled.div`
+  padding: 0px 16px;
+`;
 
 const HomePresenter = ({
   nowPlaying,
@@ -9,13 +14,35 @@ const HomePresenter = ({
   popular,
   error,
   loading,
-}) => null;
+}) =>
+  loading ? null : (
+    <Container>
+      {nowPlaying && nowPlaying.length > 0 && (
+        <Section title="Now Playing">
+          {nowPlaying.map(movie => movie.title)}
+        </Section>
+      )}
+      {upcoming && upcoming.length > 0 && (
+        <Section title="Upcoming Movies">
+          {upcoming.map(movie => movie.title)}
+        </Section>
+      )}
+      {popular && popular.length > 0 && (
+        <Section title="Popular Movies">
+          {popular.map(movie => movie.title)}
+        </Section>
+      )}
+      {topRated && topRated.length > 0 && (
+        <Section title="TopRated">{topRated.map(movie => movie.title)}</Section>
+      )}
+    </Container>
+  );
 
 HomePresenter.propTypes = {
   nowPlaying: PropTypes.array,
+  popular: PropTypes.array,
   upcoming: PropTypes.array,
   topRated: PropTypes.array,
-  popular: PropTypes.array,
   error: PropTypes.string,
   loading: PropTypes.bool.isRequired,
 };
