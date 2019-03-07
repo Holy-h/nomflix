1b8ed4aff6ba1ef35043d7e1a703e976673f550a
Bonggt <kepy1106@gmail.com>
2019-03-07 10:58:33 +0900

    react-app-polyfill

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "Components/App";
+import "react-app-polyfill/ie9";
 
 // eslint-disable-next-line no-undef
 ReactDOM.render(<App />, document.getElementById("root"));
